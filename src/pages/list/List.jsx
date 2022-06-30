ff94e75cb3cf4b1e8444ce7c9c6260f64246e46d
hemel-raihan <hemelraihan1997@gmail.com>
2022-06-30 17:26:20 +0600

    design search hotels list & single hotel details with react slider

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -5,7 +5,7 @@ import { useLocation } from "react-router-dom";
 import { useState } from "react";
 import { format } from "date-fns";
 import { DateRange } from "react-date-range";
-//import SearchItem from "../../components/searchItem/SearchItem";
+import SearchItem from "../../components/searchItem/SearchItem";
 
 const List = () => {
   const location = useLocation();
@@ -39,7 +39,7 @@ const List = () => {
                   ranges={date}
                 />
               )}
-            </div>
+             </div>
             <div className="lsItem">
               <label>Options</label>
               <div className="lsOptions">
@@ -87,7 +87,6 @@ const List = () => {
             <button>Search</button>
           </div>
           <div className="listResult">
-            {/* <SearchItem />
             <SearchItem />
             <SearchItem />
             <SearchItem />
@@ -95,7 +94,8 @@ const List = () => {
             <SearchItem />
             <SearchItem />
             <SearchItem />
-            <SearchItem /> */}
+            <SearchItem />
+            <SearchItem />
           </div>
         </div>
       </div>
